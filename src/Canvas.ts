7ec355c70b94863abf676cf1fb718a3f7de45d55
Dearest <dearest@abouttime.cn>
2020-08-12 18:27:32 +0800

    实现 drawTriangle

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -62,8 +62,23 @@ class Canvas {
     }
   }
 
+  drawTriangle(v1: Vertex, v2: Vertex, v3: Vertex) {
+    // y 从小到大 排列
+    const [va, vb, vc] = [v1, v2, v3].sort(
+      (a, b) => a.position.y - b.position.y,
+    )
+
+    const middleFactor =
+      (vb.position.y - va.position.y) / (vc.position.y - va.position.y)
+
+    const vm = va.interpolate(vc, middleFactor)
+
+    this.drawTriangle1(va, vb, vm)
+    this.drawTriangle2(vb, vm, vc)
+  }
+
   // 三角形上班部分， v1在上， v2, v3 在下， y 相等
-  drawTriangle1(v1: Vertex, v2: Vertex, v3) {
+  drawTriangle1(v1: Vertex, v2: Vertex, v3: Vertex) {
     const yTop = v1.position.y
     const yBottom = v2.position.y
 
@@ -72,6 +87,20 @@ class Canvas {
 
       const va = v1.interpolate(v2, factor)
       const vb = v1.interpolate(v3, factor)
+      this.drawScanLine(va, vb)
+    }
+  }
+
+  // 三角形上班部分， v3在下， v1, v2 在上， y 相等
+  drawTriangle2(v1: Vertex, v2: Vertex, v3: Vertex) {
+    const yTop = v1.position.y
+    const yBottom = v3.position.y
+
+    for (let y = yTop + 1; y <= yBottom; y++) {
+      const factor = (y - yTop) / (yBottom - yTop)
+
+      const va = v1.interpolate(v3, factor)
+      const vb = v2.interpolate(v3, factor)
       this.drawScanLine(va, vb)
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,27 @@ const canvas = new Canvas(ele)
 
 function __main() {
   // drawLine()
+  // drawTriangleTwoParts()
   drawTriangle()
 }
 
 function drawTriangle() {
+  const v1 = new Vertex(new Vec2(300, 200), Color.red())
+  const v2 = new Vertex(new Vec2(100, 250), Color.green())
+  const v3 = new Vertex(new Vec2(450, 350), Color.blue())
+
+  canvas.drawTriangle(v1, v2, v3)
+  canvas.render()
+}
+
+function drawTriangleTwoParts() {
   const v1 = new Vertex(new Vec2(200, 100), Color.red())
   const v2 = new Vertex(new Vec2(100, 200), Color.green())
   const v3 = new Vertex(new Vec2(300, 200), Color.blue())
+  const v4 = new Vertex(new Vec2(200, 300), Color.black())
 
   canvas.drawTriangle1(v1, v2, v3)
+  canvas.drawTriangle2(v2, v3, v4)
 
   canvas.render()
 }
